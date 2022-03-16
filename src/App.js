02f6fba6bfa4a0789a9a43171af3658eaf4f70e1
Andreu Slynchyk <andreu.slynchyk@gmail.com>
2022-03-16 20:42:43 +0200

    add routers and button list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, {Component} from "react";
 import Layout from "./hoc/Layout/Layout";
 import Quiz from "./conteiners/Quiz/Quiz";
-import classes from './Style.module.css'
+import {Route, Routes} from "react-router-dom";
+import QuizList from "./conteiners/QuizList/QuizList";
+import QuizCreator from "./conteiners/QuizCreator/QuizCreator";
+import Auth from "./conteiners/Auth/Auth";
 
 class App extends Component {
     render() {
         return (
             <div>
             <Layout>
-                <Quiz/>
+                <Routes>
+                    <Route path='/auth' element={<Auth/>}/>
+                    <Route path='/quiz-creator' element={<QuizCreator/>}/>
+                    <Route path='/quiz/:id' element={<Quiz/>}/>
+                    <Route path='/' element={<QuizList/>}/>
+                </Routes>
             </Layout>
             </div>
         );
--- src/component/Navigation/Draver/Draver.js
+++ src/component/Navigation/Draver/Draver.js
@@ -1,37 +1,50 @@
 import React, {Component} from 'react';
 import classes from './Draver.module.css'
 import BackDrop from "../../UI/BackDrop/BackDrop";
+import {NavLink} from "react-router-dom";
 
 const link = [
-    1, 2, 3
+    {to: '/', lable: 'Список', exact: true},
+    {to: '/auth', lable: 'Авторизация', exact: false},
+    {to: '/quiz-creator', lable: 'Создать тест', exact: false},
 ]
 
 
 
 class Draver extends Component {
-renderLinks() {
-    return link.map((link, index) =>{
-        return(
-            <li key={index}>
-                <a>Link: {link}</a>
-            </li>
-        )
-    })
-}
+
+    clickHandler = () =>{
+        this.props.onClose()
+    }
+
+    renderLinks() {
+        return link.map((link, index) => {
+            return (
+                <li key={index}>
+                    <NavLink
+                        to={link.to}
+                        exact={link.exact}
+                        activeClassName={classes.active}
+                        onClick={this.clickHandler}
+                    >{link.lable}</NavLink>
+                </li>
+            )
+        })
+    }
 
     render() {
-    const cls =[classes.Draver]
-        if (!this.props.isOpen){
+        const cls = [classes.Draver]
+        if (!this.props.isOpen) {
             cls.push(classes.close)
         }
         return (
             <>
-            <nav className={cls.join(' ')}>
-                <ul>
-                    {this.renderLinks()}
-                </ul>
-            </nav>
-                { this.props.isOpen ? <BackDrop onClick={this.props.onClose}/> : null}
+                <nav className={cls.join(' ')}>
+                    <ul>
+                        {this.renderLinks()}
+                    </ul>
+                </nav>
+                {this.props.isOpen ? <BackDrop onClick={this.props.onClose}/> : null}
             </>
         );
     }
